c9d9717e85cb635deba5c74fcefd73f6f41e8431
hyuk02006 <hyuk02006@naver.com>
2021-10-22 11:59:00 +0900

    Jusung Exel
    Jusung Exel

---
+++ ExcelExam/ExcelExam/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace ExcelExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Level = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Name1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Count1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "로드";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Level,
+            this.Number,
+            this.Name1,
+            this.Count1,
+            this.columnHeader10});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 73);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(473, 548);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Level
+            // 
+            this.Level.Text = "Level";
+            // 
+            // Name1
+            // 
+            this.Name1.DisplayIndex = 2;
+            this.Name1.Text = "Name";
+            // 
+            // Count1
+            // 
+            this.Count1.DisplayIndex = 3;
+            this.Count1.Text = "수량";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader8,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader11});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(558, 57);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(473, 564);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Level";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 2;
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 3;
+            this.columnHeader3.Text = "수량";
+            // 
+            // Number
+            // 
+            this.Number.DisplayIndex = 1;
+            this.Number.Text = "Number";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.DisplayIndex = 1;
+            this.columnHeader8.Text = "Number";
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader9,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(1095, 57);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(504, 564);
+            this.listView3.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView3.TabIndex = 5;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Level";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Number";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Name";
+            this.columnHeader5.Width = 87;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "파일 1수량";
+            this.columnHeader6.Width = 107;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "파일 2수량";
+            this.columnHeader7.Width = 87;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "갯수";
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "갯수";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1650, 667);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Level;
+        private System.Windows.Forms.ColumnHeader Name1;
+        private System.Windows.Forms.ColumnHeader Count1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader Number;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+    }
+}
+
---
+++ ExcelExam/ExcelExam/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace ExcelExam
+{
+    public partial class Form1 : Form
+    {
+        public class FileData
+        {
+            public string level;
+            public string Name;
+            public string Count;
+            public string Number;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<FileData> fileDatas1 = new List<FileData>();
+        List<FileData> fileDatas2 = new List<FileData>();
+        static int a = 0;
+        static int b = 0;
+        static int c = 0;
+    private void button2_Click(object sender, EventArgs e)
+        {
+            // XLSX, OleDB
+            // x64에서 실행
+            string path = @"C:\Users\BIT\source\repos\ExcelExam\ExcelExam\bin\Debug\PLM BOM_비교 1.xls";
+            string path1 = @"C:\Users\BIT\source\repos\ExcelExam\ExcelExam\bin\Debug\PLM BOM_비교 2.xls";
+            string strConn = "provider=Microsoft.ACE.OLEDB.12.0;data source=" + path +
+                ";Extended Properties='Excel 12.0 XML;HDR=No;IMEX=1';";
+            string strConn1 = "provider=Microsoft.ACE.OLEDB.12.0;data source=" + path1 +
+                ";Extended Properties='Excel 12.0 XML;HDR=No;IMEX=1';";
+
+            string sql = "select * from [목록$A7:E] " ;
+            string sql1 = "select * from [목록$A7:E] " ;
+        
+
+
+            
+            OleDbConnection conn = new OleDbConnection(strConn);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            conn.Open();
+
+            OleDbConnection conn1 = new OleDbConnection(strConn1);
+            OleDbCommand cmd1 = new OleDbCommand(sql1, conn1);
+            conn1.Open();
+
+
+            OleDbDataReader dr = cmd.ExecuteReader();
+            OleDbDataReader dr1 = cmd1.ExecuteReader();
+
+            while(dr1.Read())
+            {
+                List<string> arr1 = new List<string>();
+
+                if (dr1[0].ToString() == "3" || dr1[0].ToString() == "4")
+                {              
+                    arr1.Add(dr1[0].ToString());
+                    arr1.Add(dr1[1].ToString());
+                    arr1.Add(dr1[2].ToString());
+                    arr1.Add(dr1[4].ToString());
+                    b++;
+                    arr1.Add(b.ToString());
+
+                    ListViewItem lvi1 = new ListViewItem(arr1.ToArray());
+                    listView2.Items.Add(lvi1);
+
+                    FileData Filearr1 = new FileData();
+                    Filearr1.level = dr1[0].ToString();
+                    Filearr1.Number = dr1[1].ToString();
+                    Filearr1.Name = dr1[2].ToString();
+                    Filearr1.Count = dr1[4].ToString();
+
+                    fileDatas2.Add(Filearr1);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+
+            while (dr.Read())
+            {
+                List<string> arr = new List<string>();
+                
+
+                if (dr[0].ToString() == "3" || dr[0].ToString() == "4")
+                {
+
+                    arr.Add(dr[0].ToString());
+                    arr.Add(dr[1].ToString());
+                    arr.Add(dr[2].ToString());
+                    arr.Add(dr[3].ToString());
+                    a++;
+                    arr.Add(a.ToString());             
+                }
+                else
+                {
+                    continue;
+                }
+
+
+                ListViewItem lvi = new ListViewItem(arr.ToArray());
+                listView1.Items.Add(lvi);
+
+                FileData Filearr = new FileData();
+                Filearr.level = dr[0].ToString();
+                Filearr.Number = dr[1].ToString();
+                Filearr.Name = dr[2].ToString();
+                Filearr.Count = dr[3].ToString();
+       
+                fileDatas1.Add(Filearr);                  
+
+            }
+            for (int i = 0; i < fileDatas1.Count; i++)
+            {
+                List<string> arr2 = new List<string>();
+                arr2.Add(fileDatas1[i].level.ToString());
+                arr2.Add(fileDatas1[i].Number.ToString());
+                arr2.Add(fileDatas1[i].Name.ToString());
+                arr2.Add(fileDatas1[i].Count.ToString());       
+                for (int j = 0; j < fileDatas2.Count; j++)
+                {
+                    if (fileDatas1[i].Number.ToString() == fileDatas2[j].Number.ToString() &&fileDatas1[i].level.ToString() == fileDatas2[j].level.ToString())
+                    {
+                        arr2.Add(fileDatas2[j].Count.ToString());
+                        if (fileDatas1[i].Count.ToString() == fileDatas2[j].Count.ToString() && fileDatas1[i].Name.ToString() == fileDatas2[j].Name.ToString())
+                        {                                                 
+                            break;
+                        }
+                        else
+                        {                        
+                            ListViewItem lvi2 = new ListViewItem(arr2.ToArray());
+                            listView3.Items.Add(lvi2);
+                            break;
+                        }
+                    }
+                    if (j == fileDatas2.Count-1)
+                    {
+                        arr2.Add("File2에 존재 하지 않음");
+
+                        ListViewItem lvi2 = new ListViewItem(arr2.ToArray());
+                        listView3.Items.Add(lvi2);
+
+                    }
+                    else
+                        continue;
+                }
+            }
+            dr.Close();
+            conn.Close();
+            dr1.Close();
+            conn1.Close();
+
+        }
+    }
+}
+
